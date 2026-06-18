cd22480112f2ae80b0d5051e42828a7fe77d0268
kc <kc@local>
2026-06-18 16:36:16 +0800

    P2-3 MonacoEnvironment type; P2-6 use snippets/index.ts

--- src/renderer/src/vite-env.d.ts
+++ src/renderer/src/vite-env.d.ts
@@ -4,3 +4,9 @@ declare module '*?worker' {
   const worker: new () => Worker
   export default worker
 }
+
+declare const self: {
+  MonacoEnvironment: {
+    getWorker: (workerId: string, label: string) => Worker
+  }
+} & typeof globalThis
