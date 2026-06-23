f8262fed62f33124c843e53c173c9a7ece1288e9
boxiaolanya2008 <3520687734@qq.com>
2026-06-23 20:26:54 +0800

    refactor: remove LLM-related files and components
    Deleted cloud, context, provider, and index files related to LLM functionality, along with the AiChat component. This cleanup reduces code complexity and removes unused features.
    
    Signed-off-by: boxiaolanya2008 <3520687734@qq.com>

--- src/main/llm/index.ts
+++
@@ -1,3 +0,0 @@
-export { LLMProvider, LLMConfig, LLMProviderType, Message, ChatParams, CompleteParams, ChatChunk } from './provider'
-export { createCloudProvider } from './cloud'
-export { buildContextForChat, buildContextForCompletion, summarizeTree } from './context'
